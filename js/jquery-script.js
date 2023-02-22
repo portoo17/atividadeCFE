ca5fbe2e5be986d520ea21c612e01a14451ef394
portoo17 <119354968+portoo17@users.noreply.github.com>
2023-02-22 20:11:41 -0300

    funcionalidade jquery

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,13 @@
 //efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+    
+    $("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slidetoggle("slow");
+        $("#section-login").slidetoggle("slow");
+        $("#botao-cadastrar").hide();
+
+    });
+
+});
--- js/script.js
+++ js/script.js
@@ -12,4 +12,26 @@ window.scrollTo(
 
 //Validação de Login
 
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if(usuario == 'admin' && senha == '123456'){
+        window.location = "index.html";
+        logado = 1;
+    }
+
+    if(logado == 0){
+        alert("Usuário não cadastrado, ou dados de login incorretos!");
+    }
+
+
+}
+
 //Ativar alert no botão cadastrar
+
+function cadastro() {
+    alert("cadastrado com sucesso!");
+    window.location.href = "index.html";
+}
