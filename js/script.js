324d9ed0dc64842fe73bd24dc5d045b5d4342f9b
portoo17 <119354968+portoo17@users.noreply.github.com>
2023-02-22 19:29:38 -0300

    att
    adicionando funcionalidades ao botão de voltar ao topo e ocultando a pag de cadastro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 //efeito do botão voltar ao Topo
 
+function topo(){
+window.scrollTo(
+    {
+        top:0,
+        left:0,
+        behavior:'smooth'
+    }
+)
+}
+
 //Validação de Login
 
 //Ativar alert no botão cadastrar
